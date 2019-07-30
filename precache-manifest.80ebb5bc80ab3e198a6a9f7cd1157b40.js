844968aa4288a4b62fb8aee151117a0f23f26280
RusakArtur01 <artur6200065@gmail.com>
2019-07-30 21:36:29 +0300

    Updates

---
+++ precache-manifest.80ebb5bc80ab3e198a6a9f7cd1157b40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becfe42bdf1e1afb795ea6e5976dd7a5",
+    "url": "/touristgrodno/index.html"
+  },
+  {
+    "revision": "b37c286695399c5ef3ab",
+    "url": "/touristgrodno/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/touristgrodno/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b37c286695399c5ef3ab",
+    "url": "/touristgrodno/static/js/main.ad77c867.chunk.js"
+  },
+  {
+    "revision": "272b14ea8eca19925f85",
+    "url": "/touristgrodno/static/js/runtime~main.8acbdb75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/touristgrodno/static/media/logo.5d5d9eef.svg"
+  }
+]);
